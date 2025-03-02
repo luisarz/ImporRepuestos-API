5a516b9d60363517673dec4c26b163a98901e058
luisarz <svcomputec@gmail.com>
2025-03-01 20:30:24 -0600

    Remove unused files

--- API/resources/views/country/create.blade.php
+++
@@ -1,7 +0,0 @@
-{{--
-    @extends('layouts.app')
-
-    @section('content')
-        country.create template
-    @endsection
---}}
--- API/resources/views/country/show.blade.php
+++
@@ -1,7 +0,0 @@
-{{--
-    @extends('layouts.app')
-
-    @section('content')
-        country.show template
-    @endsection
---}}
