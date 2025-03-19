d08d1bdb1f8475577b3168f1c568766f70cf3558
luisarz <svcomputec@gmail.com>
2025-03-18 20:41:59 -0600

    Module destroy method return

--- app/Http/Controllers/Api/v1/ModuloController.php
+++ app/Http/Controllers/Api/v1/ModuloController.php
@@ -140,7 +140,7 @@ class ModuloController extends Controller
         try {
             $modulo=Modulo::findOrFail($id);
             $modulo->delete();
-            ApiResponse::success(null,'Modulo eliminado exitosamente',200);
+           return ApiResponse::success(null,'Modulo eliminado exitosamente',200);
 
         }catch (ModelNotFoundException $e){
             return ApiResponse::error(null,'Modulo no encontrado',404);
